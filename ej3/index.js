dcb5148f945df9eb5c6c22634472fa7ab047fc03
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-01-13 16:41:38 +0100

    ejercicio 3 terminado

--- ej3/index.js
+++ ej3/index.js
@@ -1,4 +1,14 @@
 const numeros = require('./numeros');
+const Logger = require('logplease');
+const logger = Logger.create('utils');
 
+console.log(numeros.esPar(2));
 console.log(numeros.esPar(3));
+console.log(numeros.esPar(101));
+console.log(numeros.esPar(201));
+console.log(numeros.esPar(202));
+console.log(numeros.esPar(100));
+
+
+
 
--- ej3/numeros.js
+++ ej3/numeros.js
@@ -1,6 +1,12 @@
+const Logger = require('logplease');
+const logger = Logger.create('utils');
+
 exports.esPar = (a) => {
     if (a % 2 == 0) {
+        logger.info('El numero es par')
         return true
-    } else {return false}
+    } else {
+        logger.error('El numero no es par')
+        return false}
 };
 
