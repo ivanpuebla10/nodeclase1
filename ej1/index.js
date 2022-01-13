85c0b5b3ce957d0fed651c6cbcd743270cb778c0
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-01-13 13:44:13 +0100

    habia subido cualquier cosa, este es el que va

--- ej1/index.js
+++ ej1/index.js
@@ -1,8 +1,1 @@
-const http = require('http');
-const uc = require('upper-case');
-
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(uc.upperCase("¡Hola burro!"));
-  res.end();
-}).listen(8080);
+console.log('Mi primer script en The Bridge!!!')
